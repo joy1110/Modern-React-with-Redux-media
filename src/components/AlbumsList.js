845714f25f775d2cc1356e8221a471fccbe30c8d
Joe <joe.lu@isobar.com>
2023-03-03 18:58:51 +0800

    進行中

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,41 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import Skeletons from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const [addAlbum, results] = useAddAlbumMutation();
+
+    const handleAddAlbum = () => {
+        addAlbum(user);
+    };
+
+    let content;
+    if (isLoading) {
+        content = <Skeletons times={3} />
+    } else if (error) {
+        content = <div>Error loading albums.</div>
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>
+
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos in the album
+            </ExpandablePanel>
+        });
+    }
+
     return (
-        <div>Albums for {user.name}</div>
+        <div>
+            <div>
+                Albums for {user.name}
+                <Button onClick={handleAddAlbum}>+ Add Album</Button>
+            </div>
+            <div>
+                {content}
+            </div>
+        </div>
     );
 }
 
